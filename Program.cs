216a7ba8d55664c7ff62d1460202dccc44e34628
Alejandro Durán García <78881781+ADG1234567@users.noreply.github.com>
2022-02-15 13:50:16 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu Numero de Control:");
+            string numCon = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Nombre:");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Clase:");
+            string clas = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Carrera:");
+            string carr = Console.ReadLine();
+
+            Console.WriteLine($"Hola {nom}, tu numero de control es: {numCon}, vas en la clase {clas} de la carrera {carr}");
+            Console.Read();
+        }
+    }
+}
